ef375553b935e5f7f9a3cd60a76739883890690a
Vinay Kumar <Vinayy1709@gmail.com>
2024-07-17 13:59:24 +0530

    NetError fix v1

--- lib/gptScriptInstance.ts
+++ lib/gptScriptInstance.ts
@@ -2,7 +2,6 @@ import { GPTScript } from "@gptscript-ai/gptscript";
 
 const g = new GPTScript({
     APIKey: process.env.OPENAI_API_KEY,
-    // APIKey: 'sk-proj-2QdSMMmtGknS3dbzUjhFT3BlbkFJ30xLMWaPyMoQIXyp59YU',
 });
 
 export default g;
